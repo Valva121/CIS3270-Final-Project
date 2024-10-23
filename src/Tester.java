874eee507f5dc9ffb03309ada064ac3845ef6b8e
Valva <valerie.edokpa@gmail.com>
2024-10-22 20:33:18 -0400

    Test run for the Final Project.

--- src/Tester.java
+++ src/Tester.java
@@ -2,7 +2,7 @@ public class Tester {
     public static void main(String [] args){
         System.out.println("---Welcome---");
         System.out.println("ehy hey ehy");
-        System.out.println("ummmm");
+        System.out.println("ummmmmmmm");
 
     }
 }
